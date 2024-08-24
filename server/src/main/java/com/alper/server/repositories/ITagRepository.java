50cea3654fb49dc907dc5095baf830ef66c13ad6
Alperen Polat <m_alperenPolat@hotmail.com>
2024-08-24 16:38:15 +0300

    create filtered query method to tag repo

--- server/src/main/java/com/alper/server/repositories/ITagRepository.java
+++ server/src/main/java/com/alper/server/repositories/ITagRepository.java
@@ -4,6 +4,10 @@ import com.alper.server.entities.Tag;
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface ITagRepository extends MongoRepository<Tag, String> {
+    List<Tag> findByNameContaining(String filter);
 }
