d8267286d50ba45f75c4f870b7d7c8399bbfbb72
Alperen Polat <m_alperenPolat@hotmail.com>
2024-08-24 13:49:05 +0300

    remove websocket example code

--- app/lib/main.dart
+++ app/lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:app/services/web_socket_service.dart';
+import 'package:app/screens/home_screen.dart';
+// import 'package:app/services/web_socket_service.dart';
 import 'package:flutter/material.dart';
 
 void main() {
@@ -6,85 +7,99 @@ void main() {
 }
 
 class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+  
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter WebSocket Demo',
-      home: MyHomePage(),
+    return const MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: SafeArea(child: HomeScreen()),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
+// WebSocket usage:
 
-class _MyHomePageState extends State<MyHomePage> {
-  final TextEditingController _controller = TextEditingController();
-  late WebSocketService _webSocketService;
-  final String _webSocketUrl = 'ws://10.0.2.2:8080/ws';
+// class MyApp extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       title: 'Flutter WebSocket Demo',
+//       home: MyHomePage(),
+//     );
+//   }
+// }
 
-  @override
-  void initState() {
-    super.initState();
-    _webSocketService = WebSocketService(_webSocketUrl);
-  }
+// class MyHomePage extends StatefulWidget {
+//   @override
+//   _MyHomePageState createState() => _MyHomePageState();
+// }
 
-  @override
-  void dispose() {
-    _webSocketService.close();
-    super.dispose();
-  }
+// class _MyHomePageState extends State<MyHomePage> {
+//   final TextEditingController _controller = TextEditingController();
+//   late WebSocketService _webSocketService;
+//   final String _webSocketUrl = 'ws://10.0.2.2:8080/ws';
 
-  void _sendMessage() {
-    final message = _controller.text;
-    _webSocketService.sendMessage(message);
-    _controller.clear();
-  }
+//   @override
+//   void initState() {
+//     super.initState();
+//     _webSocketService = WebSocketService(_webSocketUrl);
+//   }
 
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: Text('Flutter WebSocket Demo'),
-      ),
-      body: Column(
-        children: <Widget>[
-          Expanded(
-            child: StreamBuilder<String>(
-              stream: _webSocketService.messages,
-              builder: (context, snapshot) {
-                return ListView(
-                  children: [
-                    if (snapshot.hasData)
-                      ListTile(title: Text('Server: ${snapshot.data}')),
-                  ],
-                );
-              },
-            ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Row(
-              children: <Widget>[
-                Expanded(
-                  child: TextField(
-                    controller: _controller,
-                    decoration: InputDecoration(
-                      labelText: 'Enter message',
-                    ),
-                  ),
-                ),
-                IconButton(
-                  icon: Icon(Icons.send),
-                  onPressed: _sendMessage,
-                ),
-              ],
-            ),
-          ),
-        ],
-      ),
-    );
-  }
-}
+//   @override
+//   void dispose() {
+//     _webSocketService.close();
+//     super.dispose();
+//   }
+
+//   void _sendMessage() {
+//     final message = _controller.text;
+//     _webSocketService.sendMessage(message);
+//     _controller.clear();
+//   }
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       appBar: AppBar(
+//         title: Text('Flutter WebSocket Demo'),
+//       ),
+//       body: Column(
+//         children: <Widget>[
+//           Expanded(
+//             child: StreamBuilder<String>(
+//               stream: _webSocketService.messages,
+//               builder: (context, snapshot) {
+//                 return ListView(
+//                   children: [
+//                     if (snapshot.hasData)
+//                       ListTile(title: Text('Server: ${snapshot.data}')),
+//                   ],
+//                 );
+//               },
+//             ),
+//           ),
+//           Padding(
+//             padding: const EdgeInsets.all(16.0),
+//             child: Row(
+//               children: <Widget>[
+//                 Expanded(
+//                   child: TextField(
+//                     controller: _controller,
+//                     decoration: InputDecoration(
+//                       labelText: 'Enter message',
+//                     ),
+//                   ),
+//                 ),
+//                 IconButton(
+//                   icon: Icon(Icons.send),
+//                   onPressed: _sendMessage,
+//                 ),
+//               ],
+//             ),
+//           ),
+//         ],
+//       ),
+//     );
+//   }
+// }
--- app/lib/screens/home_screen.dart
+++ app/lib/screens/home_screen.dart
@@ -1,6 +1,4 @@
 import 'package:flutter/material.dart';
-import 'package:web_socket_channel/io.dart';
-import 'package:web_socket_channel/web_socket_channel.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({super.key});
@@ -10,22 +8,10 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  WebSocketChannel channel = IOWebSocketChannel.connect("wss://ws://localhost:8080/gs-guide-websocket/topic/greetings");
-  WebSocketChannel messageChannel = IOWebSocketChannel.connect("wss://ws://localhost:8080/gs-guide-websocket/hello");
-
-  void sendData() {
-    messageChannel.sink.add("mesaj");
-  }
-
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          StreamBuilder(stream: channel.stream, builder: (context, snapshot) => Padding(padding: const EdgeInsets.all(8), child: Text(snapshot.hasData ? "${snapshot.data}" : "no data"),)),
-          ElevatedButton(onPressed: sendData, child: const Text("send"))
-        ],
-      ),
+    return const Scaffold(
+      body: Center(child: Text("Home screen works!"),)
     );
   }
 }
