3236a648d3f19f3e87baa53098a0880f1d9a93b2
Alperen Polat <m_alperenPolat@hotmail.com>
2024-08-26 20:12:07 +0300

    create register screen template

---
+++ app/lib/screens/register_screen.dart
@@ -0,0 +1,109 @@
+import 'package:flutter/material.dart';
+
+class RegisterScreen extends StatefulWidget {
+  const RegisterScreen({super.key});
+
+  @override
+  State<RegisterScreen> createState() => _RegisterScreenState();
+}
+
+class _RegisterScreenState extends State<RegisterScreen> {
+  final TextEditingController _usernameController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final _formKey = GlobalKey<FormState>();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: Colors.blue.shade50,
+      appBar: AppBar(
+        foregroundColor: Colors.white,
+        backgroundColor: Colors.blue,
+        title: const Text("Sign In"),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(20.0),
+        child: Center(
+          child: Form(
+              key: _formKey,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  TextFormField(
+                    decoration: const InputDecoration(
+                        fillColor: Colors.white,
+                        filled: true,
+                        icon: Icon(Icons.person),
+                        labelText: "Username"),
+                    controller: _usernameController,
+                    validator: (value) {
+                      if (value!.trim().isEmpty) {
+                        return "Required field";
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: const InputDecoration(
+                        icon: Icon(Icons.lock),
+                        filled: true,
+                        fillColor: Colors.white,
+                        labelText: "Password"),
+                    controller: _passwordController,
+                    validator: (value) {
+                      if (value!.trim().isEmpty) {
+                        return "Required field!";
+                      }
+                      return null;
+                    },
+                  ),
+                  const SizedBox(
+                    height: 10,
+                  ),
+                  ElevatedButton(
+                      style: const ButtonStyle(
+                          backgroundColor: WidgetStatePropertyAll(Colors.blue)),
+                      onPressed: () {
+                        if (_formKey.currentState!.validate()) {
+                          try {
+                            //TODO: Submit form
+                            //TODO: Navigate to next page
+                            _usernameController.clear();
+                            _passwordController.clear();
+                          } catch (ex) {
+                            //TODO: Error handling
+                          }
+                        }
+                      },
+                      child: const Text(
+                        "Login",
+                        style: TextStyle(color: Colors.white),
+                      )),
+                  const SizedBox(height: 20),
+                  GestureDetector(
+                    onTap: () {
+                      Navigator.of(context).push(MaterialPageRoute(
+                          builder: (context) => const RegisterScreen()));
+                    },
+                    child: RichText(
+                        text: const TextSpan(
+                            text: "Already have an account? ",
+                            style: TextStyle(color: Colors.black),
+                            children: [
+                          TextSpan(
+                              text: "Go to login page.",
+                              style: TextStyle(color: Colors.blue))
+                        ])),
+                  )
+                ],
+              ),
+            ),
+        ),
+      ),
+    );
+  }
+}
